da556fbed7b9e0d0d0b3cf37bb5f6f3e898fb022
kamano905 <katsu0422.amano@gmail.com>
2024-10-15 02:13:05 +0200

    reduce verification

--- kubewarden/step1/verify.sh
+++ kubewarden/step1/verify.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-helm version
 helm repo list | grep -q kubewarden
-opa version
--- kubewarden/step2/verify.sh
+++ kubewarden/step2/verify.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 kubectl get pods -n kubewarden | grep -q kubewarden-controller
-curl localhost:5000/v2/
